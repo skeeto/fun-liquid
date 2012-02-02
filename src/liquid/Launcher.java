d269887f579aa23a27ef6989bebb6deaff73784d
Christopher Wellons <mosquitopsu@gmail.com>
2012-02-01 20:12:17 -0500

    Working balls-in-a-box thing.

---
+++ src/liquid/Launcher.java
@@ -0,0 +1,107 @@
+package liquid;
+
+import java.awt.geom.Rectangle2D;
+import java.lang.InterruptedException;
+import java.util.Random;
+import javax.swing.JFrame;
+import lombok.extern.java.Log;
+import org.jbox2d.collision.shapes.CircleShape;
+import org.jbox2d.collision.shapes.PolygonShape;
+import org.jbox2d.common.Vec2;
+import org.jbox2d.dynamics.Body;
+import org.jbox2d.dynamics.BodyDef;
+import org.jbox2d.dynamics.BodyType;
+import org.jbox2d.dynamics.FixtureDef;
+import org.jbox2d.dynamics.World;
+
+@Log
+public class Launcher {
+
+    private static final float DT = 1f / 30f; // seconds
+    private static final int V_ITERATIONS = 8;
+    private static final int P_ITERATIONS = 3;
+    private static final float WIDTH = 50f;
+    private static final float HEIGHT = 70f;
+    private static final float THICKNESS = 1f;
+    private static final Vec2 GRAVITY = new Vec2(0, -10);
+    private static final Rectangle2D VIEW =
+        new Rectangle2D.Float(WIDTH / -2, HEIGHT / -2, WIDTH, HEIGHT);
+
+    /* Balls */
+    private static final int BALLS = 20;
+    private static final float BALL_DENSITY = 1f;
+    private static final float BALL_FRICTION = 0.1f;
+    private static final float BALL_RESTITUTION = 0.6f;
+
+    public static void main(String[] args) {
+        /* Fix for poor OpenJDK performance. */
+        System.setProperty("sun.java2d.pmoffscreen", "false");
+
+        World world = new World(GRAVITY, true);
+        Viewer viewer = new Viewer(world, VIEW);
+        JFrame frame = new JFrame("Fun Liquid");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(viewer);
+        frame.setResizable(false);
+        frame.pack();
+        frame.setVisible(true);
+
+        /* Set up the containment box. */
+        buildContainer(world);
+
+        /* Add a ball. */
+        Random rng = new Random();
+        for (int i = 0; i < BALLS; i++) {
+            addBall(world,
+                    (rng.nextFloat() - 0.5f) * WIDTH,
+                    (rng.nextFloat() - 0.5f) * HEIGHT);
+        }
+
+        final int dtms = (int) (DT * 1000.0);
+        while (true) {
+            world.step(DT, V_ITERATIONS, P_ITERATIONS);
+            viewer.repaint();
+            try {
+                Thread.sleep(dtms);
+            } catch (InterruptedException e) {
+                log.warning("interrupted");
+            }
+        }
+    }
+
+    private static void buildContainer(World world) {
+        BodyDef def = new BodyDef();
+        PolygonShape box = new PolygonShape();
+        Body side;
+
+        def.position = new Vec2(WIDTH / 2, 0);
+        box.setAsBox(THICKNESS / 2, HEIGHT / 2);
+        world.createBody(def).createFixture(box, 0f);
+
+        def.position = new Vec2(-WIDTH / 2, 0);
+        box.setAsBox(THICKNESS / 2, HEIGHT / 2);
+        world.createBody(def).createFixture(box, 0f);
+
+        def.position = new Vec2(0, HEIGHT / 2);
+        box.setAsBox(WIDTH / 2, THICKNESS / 2);
+        world.createBody(def).createFixture(box, 0f);
+
+        def.position = new Vec2(0, -HEIGHT / 2);
+        box.setAsBox(WIDTH / 2, THICKNESS / 2);
+        world.createBody(def).createFixture(box, 0f);
+    }
+
+    private static void addBall(World world, float x, float y) {
+        BodyDef def = new BodyDef();
+        def.position = new Vec2(x, y);
+        def.type = BodyType.DYNAMIC;
+        CircleShape circle = new CircleShape();
+        circle.m_radius = 1f;
+        FixtureDef mass = new FixtureDef();
+        mass.shape = circle;
+        mass.density = BALL_DENSITY;
+        mass.friction = BALL_FRICTION;
+        mass.restitution = BALL_RESTITUTION;
+        world.createBody(def).createFixture(mass);
+    }
+}
---
+++ src/liquid/Viewer.java
@@ -0,0 +1,90 @@
+package liquid;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.awt.Graphics;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Path2D;
+import java.awt.geom.Rectangle2D;
+import javax.swing.JComponent;
+import org.jbox2d.collision.shapes.CircleShape;
+import org.jbox2d.collision.shapes.PolygonShape;
+import org.jbox2d.collision.shapes.Shape;
+import org.jbox2d.common.Vec2;
+import org.jbox2d.dynamics.Body;
+import org.jbox2d.dynamics.Fixture;
+import org.jbox2d.dynamics.World;
+
+public class Viewer extends JComponent {
+
+    private static final Color BACKGROUND = Color.BLACK;
+    private static final Color FOREGROUND = Color.WHITE;
+
+    private static final long serialVersionUID = 1L;
+
+    private static final float SCALE = 5f;
+
+    private final World world;
+    private final Rectangle2D view;
+
+    public Viewer(World world, Rectangle2D view) {
+        this.world = world;
+        this.view = view;
+        Dimension size = new Dimension((int) (view.getWidth() * SCALE),
+                                       (int) (view.getHeight() * SCALE));
+        setPreferredSize(size);
+    }
+
+    @Override
+    public void paintComponent(Graphics graphics) {
+        Graphics2D g = (Graphics2D) graphics;
+        g.setColor(BACKGROUND);
+        g.fillRect(0, 0, getWidth(), getHeight());
+        g.translate(getWidth() / 2, getHeight() / 2);
+        g.scale(SCALE, -SCALE);
+
+        g.setColor(FOREGROUND);
+        Body body = world.getBodyList();
+        while (body != null) {
+            Vec2 pos = body.getPosition();
+            float angle = body.getAngle();
+            Fixture fixture = body.getFixtureList();
+            while (fixture != null) {
+                Shape shape = fixture.getShape();
+                if (shape instanceof CircleShape) {
+                    draw(g, pos, (CircleShape) shape);
+                } else if (shape instanceof PolygonShape) {
+                    draw(g, pos, angle, (PolygonShape) shape);
+                } else {
+                    System.out.println("Cannot draw shape: " + shape);
+                }
+                fixture = fixture.getNext();
+            }
+            body = body.getNext();
+        }
+    }
+
+    private void draw(Graphics2D g, Vec2 pos, CircleShape s) {
+        Ellipse2D circle = new Ellipse2D.Float(pos.x - s.m_radius,
+                                               pos.y - s.m_radius,
+                                               s.m_radius * 2, s.m_radius * 2);
+        g.fill(circle);
+    }
+
+    private void draw(Graphics2D g, Vec2 pos, float angle, PolygonShape s) {
+        Path2D path = new Path2D.Float();
+        Vec2 first = s.getVertex(0);
+        path.moveTo(first.x, first.y);
+        for (int i = 1; i < s.getVertexCount(); i++) {
+            Vec2 v = s.getVertex(i);
+            path.lineTo(v.x, v.y);
+        }
+        path.closePath();
+        AffineTransform at = new AffineTransform();
+        at.translate(pos.x, pos.y);
+        at.rotate(angle);
+        g.fill(at.createTransformedShape(path));
+    }
+}
