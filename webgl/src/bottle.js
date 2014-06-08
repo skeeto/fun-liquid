4bdb27db6651f6923d949b0dde09eea995353a24
Christopher Wellons <wellons@nullprogram.com>
2014-06-08 18:58:15 -0400

    Dump old canvas 2D code.

--- webgl/src/bottle.js
+++ webgl/src/bottle.js
@@ -17,53 +17,49 @@ function Bottle(canvas) {
         this.addBall();
     }
 
-    /* Rendering */
     this.fps = new FPS();
-    try {
-        var gl = this.gl = Igloo.getContext(canvas, true);
-        gl.enable(gl.BLEND);
-        gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
-        gl.disable(gl.DEPTH_TEST);
-        gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-
-        function program(v, f) {
-            return new Igloo.Program(gl, 'src/' + v, 'src/' + f);
-        }
-        this.programs = {
-            balls:     program('ball.vert', 'ball.frag'),
-            blur:      program('identity.vert', 'blur.frag'),
-            threshold: program('identity.vert', 'threshold.frag'),
-            spikes:    program('identity.vert', 'color.frag')
-        };
-
-        var spikes = [];
-        var w = this.width, h = this.height;
-        this.polys.forEach(function(poly) {
-            var x = poly.pos.get_x(), y = poly.pos.get_y();
-            poly.verts.forEach(function(vert) {
-                spikes.push((vert.get_x() + x) / w * 2);
-                spikes.push((vert.get_y() + y) / h * 2);
-            });
-        });
-
-        this.buffers = {
-            balls:  new Igloo.Buffer(gl),
-            spikes: new Igloo.Buffer(gl, new Float32Array(spikes)),
-            quad:   new Igloo.Buffer(gl, new Float32Array([
-                    -1, -1, 1, -1, -1, 1, 1, 1
-            ]))
-        };
-
-        this.fbo = gl.createFramebuffer();
-        this.textures = {
-            front: this.createTexture(),
-            back:  this.createTexture()
-        };
-    } catch (e) {
-        this.programs = null;
-        this.ctx = canvas.getContext('2d');
-        console.warn('Failed to init WebgL: ' + e);
+    var gl = this.gl = Igloo.getContext(canvas);
+    if (gl == null) {
+        alert('Could not initialize WebGL!');
     }
+    gl.enable(gl.BLEND);
+    gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+    gl.disable(gl.DEPTH_TEST);
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+    function program(v, f) {
+        return new Igloo.Program(gl, 'src/' + v, 'src/' + f);
+    }
+    this.programs = {
+        balls:     program('ball.vert', 'ball.frag'),
+        blur:      program('identity.vert', 'blur.frag'),
+        threshold: program('identity.vert', 'threshold.frag'),
+        spikes:    program('identity.vert', 'color.frag')
+    };
+
+    var spikes = [];
+    var w = this.width, h = this.height;
+    this.polys.forEach(function(poly) {
+        var x = poly.pos.get_x(), y = poly.pos.get_y();
+        poly.verts.forEach(function(vert) {
+            spikes.push((vert.get_x() + x) / w * 2);
+            spikes.push((vert.get_y() + y) / h * 2);
+        });
+    });
+
+    this.buffers = {
+        balls:  new Igloo.Buffer(gl),
+        spikes: new Igloo.Buffer(gl, new Float32Array(spikes)),
+        quad:   new Igloo.Buffer(gl, new Float32Array([
+                -1, -1, 1, -1, -1, 1, 1, 1
+        ]))
+    };
+
+    this.fbo = gl.createFramebuffer();
+    this.textures = {
+        front: this.createTexture(),
+        back:  this.createTexture()
+    };
 }
 
 Bottle.WIDTH = 50;
@@ -185,30 +181,7 @@ Bottle.prototype.addBall = function(pos) {
     this.balls.push(this.world.CreateBody(def).CreateFixture(mass));
 };
 
-Bottle.prototype.draw = function(f) {
-    try {
-        var w = this.ctx.canvas.width,
-            h = this.ctx.canvas.height;
-        this.ctx.fillStyle = 'black';
-        this.ctx.fillRect(0, 0, w, h);
-        this.ctx.save();
-        this.ctx.translate(w / 2, h / 2);
-        this.ctx.scale(w / this.width, h / this.height);
-        f(this.ctx);
-    } finally {
-        this.ctx.restore();
-    }
-};
-
 Bottle.prototype.render = function() {
-    if (this.programs == null) {
-        this.render2D();
-    } else {
-        this.renderGL();
-    }
-};
-
-Bottle.prototype.renderGL = function() {
     var gl = this.gl;
     var w = this.gl.canvas.width, h = this.gl.canvas.height;
     var sx = w / this.width * 2, sy = h / this.height * 2;
@@ -258,30 +231,6 @@ Bottle.prototype.renderGL = function() {
         .attrib('position', this.buffers.spikes, 2)
         .uniform('color', vec4(0.5, 0.5, 0.5, 1.0))
         .draw(gl.TRIANGLES, this.polys.length * 3);
-};
-
-Bottle.prototype.render2D = function() {
-    var balls = this.balls, polys = this.polys;
-    this.draw(function(ctx) {
-        ctx.fillStyle = 'white';
-        balls.forEach(function(ball) {
-            var pos = ball.GetBody().GetPosition();
-            var x = pos.get_x(), y = pos.get_y();
-            ctx.beginPath();
-            ctx.arc(x, y, Bottle.BALL_RADIUS, 0, 2 * Math.PI);
-            ctx.fill();
-        });
-        ctx.fillStyle = 'gray';
-        polys.forEach(function(poly) {
-            var v = poly.verts, p = poly.pos;
-            ctx.beginPath();
-            ctx.moveTo(v[0].get_x() + p.get_x(), v[0].get_y() + p.get_y());
-            for (var i = 1; i < v.length; i++) {
-                ctx.lineTo(v[i].get_x() + p.get_x(), v[i].get_y() + p.get_y());
-            }
-            ctx.fill();
-        });
-    });
 };
 
 Bottle.prototype.step = function() {
